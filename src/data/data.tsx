1cd4ba77db33e1d441f7fd356676815ee7e2e207
Sebastien Naze <sebastin@QIMR18963.local>
2023-07-14 19:51:21 +1000

    Bastille day 2023

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -14,12 +14,12 @@ import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
-import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+import plosCB from '../images/portfolio/plosCB.png';
+import review from '../images/portfolio/review.jpg';
+import harmonics from '../images/portfolio/harmonics.jpg';
+import concussion from '../images/portfolio/concussion.jpg';
+import neurofilament from '../images/portfolio/neurofilament.webp';
+import deeplearning from '../images/portfolio/deeplearning.jpg';
 import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
 import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
 import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Sebastien Naze Webpage',
+  description: "Site built with Tim Baker's react resume template",
 };
 
 /**
@@ -69,18 +69,16 @@ export type SectionId = typeof SectionId[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `Sebastien Naze`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I am a <strong className="text-stone-100">Computational Neuroscientist</strong>, currently working
+        at the Queensland Institute of Medical Research in the Department of <strong className="text-stone-100">Brain Modeling  
+        and Clinical Brain Networks</strong>.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my spare time, I ride <strong className="text-stone-100">bikes</strong> and walk <strong className="text-stone-100">slacklines</strong>.
       </p>
     </>
   ),
@@ -104,16 +102,14 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `Senior Research Scientist in Computational Neuroscience. `,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Brisbane, Australia', Icon: MapIcon},
+    {label: 'Age', text: '34', Icon: CalendarIcon},
+    {label: 'Citizenship', text: 'French', Icon: FlagIcon},
+    {label: 'Interests', text: 'Brains, Minds & Computers', Icon: SparklesIcon},
+    {label: 'PhD', text: '2015', Icon: AcademicCapIcon},
+    {label: 'Employments', text: 'QIMR Berghofer', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -125,67 +121,71 @@ export const skills: SkillGroup[] = [
     name: 'Spoken languages',
     skills: [
       {
-        name: 'English',
+        name: 'French',
         level: 10,
       },
       {
-        name: 'French',
-        level: 4,
+        name: 'English',
+        level: 9,
       },
       {
         name: 'Spanish',
-        level: 3,
-      },
-    ],
-  },
-  {
-    name: 'Frontend development',
-    skills: [
-      {
-        name: 'React',
-        level: 9,
-      },
-      {
-        name: 'Typescript',
-        level: 7,
-      },
-      {
-        name: 'GraphQL',
-        level: 6,
-      },
-    ],
-  },
-  {
-    name: 'Backend development',
-    skills: [
-      {
-        name: 'Node.js',
-        level: 8,
-      },
-      {
-        name: 'Rust',
         level: 5,
       },
       {
-        name: 'Golang',
+        name: 'Portuguese',
+        level: 3,
+      },
+    ],
+  },
+  {
+    name: 'Programming Languages',
+    skills: [
+      {
+        name: 'Python',
+        level: 9,
+      },
+      {
+        name: 'C/C++',
+        level: 6,
+      },
+      {
+        name: 'Java',
         level: 4,
       },
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Computing Skills',
     skills: [
       {
-        name: 'React Native',
+        name: 'Parallel and Distributed Computing',
+        level: 8,
+      },
+      {
+        name: 'Databases',
+        level: 5,
+      },
+      {
+        name: 'Cloud Engineering',
+        level: 3,
+      },
+    ],
+  },
+  {
+    name: 'Data Analysis',
+    skills: [
+      {
+        name: 'Graph Signal Processing',
         level: 9,
       },
       {
-        name: 'Flutter',
-        level: 4,
+        name: 'Machine Learning',
+        level: 8,
       },
       {
-        name: 'Swift',
-        level: 3,
+        name: 'Fourier/Wavelets transforms',
+        level: 7,
       },
     ],
   },
@@ -196,40 +196,40 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage1,
+    title: 'Seizure Dynamics',
+    description: 'Top-down computational modeling of seizure dynamics using coupled spiking and bursting neural network derived from the Epileptor model.',
+    url: 'https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1004209',
+    image: plosCB,
   },
   {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage2,
+    title: 'Review: Modern Concepts in Seizure Modeling',
+    description: 'Co-authored review on state-of-art seizure models.',
+    url: 'https://www.sciencedirect.com/science/article/abs/pii/B9780124186934000066?via%3Dihub',
+    image: review,
   },
   {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage3,
+    title: 'Connectome Harmonics',
+    description: 'Sensitivity analysis of the connectome harmoics framework.',
+    url: 'https://www.sciencedirect.com/science/article/pii/S1053811920308508',
+    image: harmonics,
   },
   {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage4,
+    title: 'Connectome Eigenmodes: Concussion',
+    description: 'Applying the connectome eigenmodes framework to post-concussion/traumatic brain injury data from University Health Network, Toronto.',
+    url: 'https://ieeexplore.ieee.org/abstract/document/9044786',
+    image: concussion,
   },
   {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage5,
+    title: 'Connectome Eigenmodes: Neurofilament',
+    description: 'Use connectome eigenmodes framework to predict a blood biomarker.',
+    url: 'https://www.nature.com/articles/s43856-021-00065-5',
+    image: neurofilament,
   },
   {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage6,
+    title: 'Deep learning applied to TMS Evoked Potentials',
+    description: 'Classification of TMS evoked potentials using ERP time signatures and SVM versus deep learning.',
+    url: 'https://ieeexplore.ieee.org/abstract/document/8857583',
+    image: deeplearning,
   },
   {
     title: 'Project title 7',
@@ -268,41 +268,60 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: '2015',
+    location: 'Aix-Marseille Université, Provence, France',
+    title: 'PhD in Computational Neursocience',
+    content: <p>Thesis entitled "Multiscale Modeling of Epileptic Seizure Dynamics" under the supervision of Dr Viktor Jirsa (theoritical physics)
+        and Dr Christophe Bernard (neurophysiology). 
+        Expertise acquired: dynamical systems theory, high performance computing, neuroscience.
+    </p>,
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: '2011',
+    location: 'Vrije Universiteit, Amsterdam, The Netherlands',
+    title: 'MSc in Information Science',
+    content: <p>Master degree with specialization in data engineering, business intelligence and machine learning. 
+        Master thesis entitled "Agent modeling of post-traumatic stress disorders" under supervision of Professor Jan Treur.</p>,
+  },
+  {
+    date: '2009',
+    location: 'Heriot-Watt Universty, Edinburgh, Scotland',
+    title: 'BSc in Computer Science',
+    content: <p>Bachelor Degree with specialization in software engineering & databases.</p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: '2021 - Present',
+    location: 'Queensland Institute of Medical Research, Brisbane, Australia',
+    title: 'Research Scientist',
     content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+      <p>Data engineering, analysis of multimodal dataset, and multiscale modeling of neurostimulation in obsessive-compulsive disorders.</p>
+    ),
+  },
+  {
+    date: '2019 - 2021',
+    location: 'IBM Research, Melbourne, Australia',
+    title: 'Research Scientist',
+    content: (
+      <p> Design and implementation of an interface between brain modeling and machine learning algorithms in Python and C++.
+        Parameter optimization using evolutionary algorithms. Integration of libraries into service architecture for cloud computing.
+        International team of 4-12 people operating from multiple countries.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
-    content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
-    ),
+        date: '2016 - 2019',
+        location: 'IBM Research, New York, United States',
+        title: 'Post-doctoral Researcher',
+        content: (
+            <p> Delivered IBM Solutions in brain modeling to clients (CHDI, Pfizer) investigating Huntington’s disease phenotypes in-silico. 
+            Delivered quarterly reports with client presentations and ThinkLab demonstrations at client’s events. 
+            Developed software patches for IBM Neural Graph Simulator.
+            Served as IBM team co-lead on joint collaborative research projects with Stanford University Department of Medical Neuroscience and University Health Network, Toronto.
+            </p>
+        ),
   },
 ];
 
@@ -340,23 +359,18 @@ export const contact: ContactSection = {
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'sebastien.naze@gmail.com',
+      href: 'mailto:sebastien.naze@gmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
-    },
-    {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: 'The Gap, Brisbane, Australia',
+      href: 'https://www.google.com/maps/@-27.4482413,152.9328704,6315m/data=!3m1!1e3!5m1!1e4?entry=ttu',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'sebnaze',
+      href: 'https://github.com/sebnaze',
     },
   ],
 };
@@ -365,9 +379,6 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/sebnaze'},
   {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/tbakerx/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
 ];
