62e48c03f23a1a77946377eaab510de9bd3c41a8
sebnaze <sebastien.naze@gmail.com>
2024-02-08 13:07:01 +0100

    Updates

---
+++ static/1QFxuz1T4m-S7XGqthz7V/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/913-016dc1ccecb53621.js","static/chunks/pages/index-87323d4b237a7273.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
